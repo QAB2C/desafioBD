d67065b443e89b024d4ae82428df42dc0d607a5d
Vmlopez88 <48725278+vmlopez88@users.noreply.github.com>
2019-03-28 11:14:40 -0500

    Update switchear.java

--- Tareas/switchear.java
+++ Tareas/switchear.java
@@ -1,12 +1,18 @@
+/**
+ *
+ * @author BMXDDT005858
+ * Se crea un metodo llamado imprimir en la que por posicion intercambia las variables que se muestran
+ */
 public class switchear {
+
     public static void main(String[] args) {
 
         int a = 3;
         int b = 7;
 
-        imprimir(a,b); //original
+        imprimir(a, b); //original
         System.out.println("-----------------------------");
-        imprimir(b,a); //switch
+        imprimir(b, a); //switch
     }
 
     public static void imprimir(int a, int b) {
